9f93899f36c4768bede947891bc3fb5d3a376c35
KAVAIIKO <nyakatyan@outlook.com>
2022-04-08 03:20:33 +0300

    Поправлено форматирование Добавлена обработка ошибки деления на 0

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -12,19 +12,14 @@ namespace Calc
 
             while ((Line = Console.ReadLine()) != null)
             {
-                if (Line.ToLower() == "exit") return 0;
+                if (Line.ToLower() == "exit")
+                {
+                    return 0;
+                }
                 try
                 {
-                    var (LeftNum, Operation, RightNum) = PharseArgs(Line);
-                    double ResultNum = Operation switch
-                    {
-                        '+' => LeftNum + RightNum,
-                        '/' => LeftNum / RightNum,
-                        '*' => LeftNum * RightNum,
-                        '-' => LeftNum - RightNum,
-                        _ => throw new Exception("В PharseArgs Добавлена новая операция, однако неизвестно что с ней делать")
-                    };
-                    Console.WriteLine("{0} {1} {2} = {3}", LeftNum, Operation, RightNum, ResultNum);
+                    double ResultNum = Calc(Line);
+                    Console.WriteLine(ResultNum);
                 }
                 catch (Exception Error)
                 {
@@ -32,6 +27,35 @@ namespace Calc
                 }
             }
             return 0;
+        }
+
+        static double Calc(string Line)
+        {
+
+            (double LeftNum, char Operation, double RightNum) = PharseArgs(Line);
+            double ResultNum = 0;
+            if (Operation == '+')
+            {
+                ResultNum = LeftNum + RightNum;
+            }
+            else if (Operation == '-')
+            {
+                ResultNum = LeftNum - RightNum;
+            }
+            else if (Operation == '*')
+            {
+                ResultNum = LeftNum * RightNum;
+            }
+            else if (Operation == '/')
+            {
+                if (RightNum == 0)
+                {
+                    throw new Exception("Деление на ноль");
+                }
+                ResultNum = LeftNum / RightNum;
+            }
+
+            return ResultNum;
         }
 
         static (double, char, double) PharseArgs(string Line)
@@ -51,37 +75,51 @@ namespace Calc
                         case '-':
                         case '*':
                         case '/':
-                            Pharse.Add(c.ToString());
-                            Pharse.Add("");
-                            break;
+                            {
+                                Pharse.Add(c.ToString());
+                                Pharse.Add("");
+                                break;
+                            }
                         default:
-                            Pharse[^1] += c;
-                            break;
+                            {
+                                Pharse[^1] += c;
+                                break;
+                            }
                     }
                 }
 
                 for (int i = Pharse.Count - 1; i >= 0; i--)
                 {
                     Pharse[i] = Pharse[i].Trim();
-                    if (Pharse[i] == "") Pharse.RemoveAt(i);
+                    if (Pharse[i] == "")
+                    {
+                        Pharse.RemoveAt(i);
+                    }
                 }
                 return Pharse;
             }
+
             List<string> PharseArgs = PharseLine(Line);
 
-            foreach (string arg in PharseArgs) Console.WriteLine(arg);
-
-            if (PharseArgs.Count != 3) throw new Exception("Данные не соответствуют формату: [Число] [Операция(/ * - +)] [Число]");
-            if (!double.TryParse(PharseArgs[0], out double LeftNum)) throw new Exception("Левый аргумент не является числом");
+            if (PharseArgs.Count != 3)
+            {
+                throw new Exception("Данные не соответствуют формату: [Число] [Операция(+ - * /)] [Число]");
+            }
+            if (!double.TryParse(PharseArgs[0], out double LeftNum))
+            {
+                throw new Exception("Левый аргумент не является числом");
+            }
             char Operation = PharseArgs[1].Trim() switch
             {
                 "+" => '+',
                 "-" => '-',
                 "*" => '*',
                 "/" => '/',
-                _ => throw new Exception("Неопознанная операция")
             };
-            if (!double.TryParse(PharseArgs[2], out double RightNum)) throw new Exception("Правый аргумент не является числом");
+            if (!double.TryParse(PharseArgs[2], out double RightNum))
+            {
+                throw new Exception("Правый аргумент не является числом");
+            }
             return (LeftNum, Operation, RightNum);
         }
     }
